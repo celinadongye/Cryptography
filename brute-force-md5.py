1814fc6917e2a218bc322da08b85ed9851d7d594
celinadong <celinady25@gmail.com>
2020-02-26 11:45:17 +0000

    Ex 3.1

---
+++ brute-force-md5.py
@@ -0,0 +1,10 @@
+import ascii
+import string
+
+
+def brute_force():
+    f = open("rockyou-samples.md5.txt", "r")
+    print(f.read())
+    string.ascii_uppercase
+    
+    # print(ascii.charlist())
