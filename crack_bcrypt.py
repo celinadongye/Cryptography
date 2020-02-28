4168f70faf9c7e912f0d7b0e9bd69121018b7592
celinadong <celinady25@gmail.com>
2020-02-28 20:19:03 +0000

    Bcrypt keeps giving me error

--- crack_bcrypt.py
+++ crack_bcrypt.py
@@ -1,29 +1,45 @@
 #!/usr/bin/env python
+from bcrypt import hashpw
 import bcrypt
 from collections import Counter
 
 def crack_bcrypt():
     file = "/afs/inf.ed.ac.uk/group/teaching/compsec/cw2/password-cracking/rockyou-samples.bcrypt.txt"
-    hashes = [line.rstrip('\n') for line in open(file)]
-    # print(hashes)
+    # salted_hashes = [line.rstrip('\n') for line in open(file)]
+    # salts = [(line.rstrip('\n')).split("$")[2] for line in salted_hashes]
+    salted_hashes = [(line.rstrip('\n')).split("$")[3] for line in open(file)]
+    salts = [line[:22].encode() for line in salted_hashes]
+    hashes = [line[22:] for line in salted_hashes]
 
-    password = "123456"
+    password = u"123456"
 
-    # salt = bcrypt.gensalt()
-    # hashed = bcrypt.hashpw('secret'.encode(), salt)
-    # print(hashed.find(salt))
-    
-    # print(hashed == bcrypt.hashpw('secret'.encode(), hashed))
-
-    # encoded_pass = bcrypt.hashpw(password.encode(),bcrypt.gensalt())
+    counter = 0
     write_file = open("bcrypt-lines.txt", "w")
     for idx,h in enumerate(hashes):
-        encoded_pass = bcrypt.hashpw(password.encode(), h)
-        if h == encoded_pass:
-            print("hello")
-            write_file.write((idx+1) + "\n")
+        if counter <= 5:
+            encoded_pass = bcrypt.hashpw(password.encode(), h)
+            if encoded_pass == h:
+            # if bcrypt.checkpw(password.encode('utf-8'), h):
+                counter += 1
+                write_file.write((idx+1) + "\n")
+        else:
+            break
     write_file.close()
+
+    # write_file = open("bcrypt-lines.txt", "w")
+    # for idx,h in enumerate(hashes):
+    #     encoded_pass = bcrypt.hashpw(password.encode(), h)
+    #     if h == encoded_pass:
+    #         print("hello")
+    #         write_file.write((idx+1) + "\n")
+    # write_file.close()
 
 if __name__ == "__main__":
     crack_bcrypt()
-    #$2b$12$NC7sY22M87uYqIxfcpkpfue/JXmlg4AgkZuhdkupaL2KixENhAUl2
+    # 2b - bcrypt algorithm version
+    # 12 - cost factor, 2^12 iterations of the key derivation function
+    # 16 bytes (22 chars) - salt
+    # 31 chars - ciphertext (password) for authentication
+
+    #$2b$12$DpvRvjJs4pAj4LMNyFklm.Qv2hcKQgOzwAVJbfkg//mBvEXqde1mq
+    #$2b$12$kzBFIef2qY7IIQE0t2UByucY8SjlI3mK lBPrpOqflykxoOpZ6sHeW
