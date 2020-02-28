27495167bed99fb26c0dba13a45393cd6fde8bec
celinadong <celinady25@gmail.com>
2020-02-28 21:05:36 +0000

    Finished password cracking

--- crack_bcrypt.py
+++ crack_bcrypt.py
@@ -1,38 +1,25 @@
-#!/usr/bin/env python
-from bcrypt import hashpw
 import bcrypt
 from collections import Counter
 
 def crack_bcrypt():
     file = "/afs/inf.ed.ac.uk/group/teaching/compsec/cw2/password-cracking/rockyou-samples.bcrypt.txt"
-    # salted_hashes = [line.rstrip('\n') for line in open(file)]
-    # salts = [(line.rstrip('\n')).split("$")[2] for line in salted_hashes]
-    salted_hashes = [(line.rstrip('\n')).split("$")[3] for line in open(file)]
-    salts = [line[:22].encode() for line in salted_hashes]
-    hashes = [line[22:] for line in salted_hashes]
-
-    password = u"123456"
+    salted_hashes = [line.rstrip('\n') for line in open(file)]
+    # salts = [line[:22].encode() for line in salted_hashes]
+    # hashes = [line[22:] for line in salted_hashes]
 
     counter = 0
     write_file = open("bcrypt-lines.txt", "w")
-    for idx,h in enumerate(hashes):
-        if counter <= 5:
-            encoded_pass = bcrypt.hashpw(password.encode(), h)
-            if encoded_pass == h:
-            # if bcrypt.checkpw(password.encode('utf-8'), h):
+    for idx,h in enumerate(salted_hashes):
+        if counter < 5:
+            # encoded_pass = bcrypt.hashpw(password.encode('utf-8'), salts[idx])
+            # if encoded_pass == h:
+            if bcrypt.checkpw(b'123456', h.encode()):
+                print("Line: " + str(idx))
                 counter += 1
-                write_file.write((idx+1) + "\n")
+                write_file.write(str(idx+1) + " ")
         else:
             break
     write_file.close()
-
-    # write_file = open("bcrypt-lines.txt", "w")
-    # for idx,h in enumerate(hashes):
-    #     encoded_pass = bcrypt.hashpw(password.encode(), h)
-    #     if h == encoded_pass:
-    #         print("hello")
-    #         write_file.write((idx+1) + "\n")
-    # write_file.close()
 
 if __name__ == "__main__":
     crack_bcrypt()
