899bf4b0949b5e29f1ee10f3df1c9c1638f09d02
celinadong <celinady25@gmail.com>
2020-02-28 13:20:27 +0000

    Finished salted sha1

--- salted_sha1.py
+++ salted_sha1.py
@@ -3,13 +3,11 @@ from collections import Counter
 
 def salted_sha1():
     file = "/afs/inf.ed.ac.uk/group/teaching/compsec/cw2/password-cracking/rockyou-samples.sha1-salt.txt"
-    hashes = [(line.rstrip('\n')).split("$")[3] for line in open(file)]
-    print(len(hashes))
+    salted_hash = [line.rstrip('\n') for line in open(file)]
+    salts = [(line.rstrip('\n')).split("$")[2] for line in salted_hash]
+    hashes = [(line.rstrip('\n')).split("$")[3] for line in salted_hash]
+    # TODO: all counters are 1???
     hash_count = Counter(hashes)
-
-    # common_passwords = ["123456", "12345", "123456789", "password", "iloveyou", "princess", "1234567", "rockyou",
-    #                "12345678", "abc123", "nicole", "daniel", "babygirl", "monkey", "lovely", "jessica", "654321"
-    #                "michael", "ashley", "qwerty", "111111", "iloveu", "000000", "michelle", "tigger"]
 
     common_passwords = ["123456", "12345", "123456789", "password", "iloveyou",
                         "princess", "1234567", "rockyou", "12345678", "abc123",
@@ -17,17 +15,26 @@ def salted_sha1():
                         "jessica", "654321", "michael", "ashley", "qwerty",
                         "111111", "iloveu", "000000", "michelle", "tigger"]
 
+    # All possible pairs of salts and passwords
+    salted_passwords = [[salt, pwd] for salt in salts for pwd in common_passwords]
+
+    # write_file = open("salt-cracked.txt", "w")
+    # for (s, p) in salted_passwords:
+    #     encoded_pass = sha1((s + p).encode()).hexdigest()
+    #     if (encoded_pass in hash_count):
+    #         write_file.write(str(hash_count[encoded_pass]) + "," + p + "\n")
+    # write_file.close()
+
+    counts = {i:0 for i in common_passwords}
     write_file = open("salt-cracked.txt", "w")
-    for p in common_passwords:
-        encoded_pass = sha1(p.encode()).hexdigest()
-        print(encoded_pass in hash_count)
+    for (s, p) in salted_passwords:
+        encoded_pass = sha1((s + p).encode()).hexdigest()
         if (encoded_pass in hash_count):
-            print("hello")
-            write_file.write(str(hash_count[encoded_pass]) + "," + p + "\n")
+            counts[p] += 1
+    for password in counts:
+        write_file.write(str(counts[password]) + "," + password + "\n")
     write_file.close()
+
 
 if __name__ == "__main__":
     salted_sha1()
-    #99996b911567c83cce17cdf194f314975c57ddf1
-    #5ee22b34e4ea6b8f32dd9bdfc38ce1c73e37205d
-
