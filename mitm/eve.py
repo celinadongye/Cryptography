726a87f76b725e5eff1b29dc89fd03579e49dd8c
celinadong <celinady25@gmail.com>
2020-02-29 15:44:56 +0000

    Relay works

--- mitm/eve.py
+++ mitm/eve.py
@@ -9,47 +9,47 @@ player = sys.argv[0].split('.', 1)[0]
 flag = sys.argv[1]
 # whenever Bob sends a message, read it in 'buffer_original'
 # Connect with Bob and perform DHKE
-socket_bob, aes = setup('alice', BUFFER_DIR, BUFFER_FILE_NAME)
+socket_bob, aes_bob = setup('alice', BUFFER_DIR, BUFFER_FILE_NAME)
 
 dialog.think('Eve thinks: "Nice, getting there..."')
 os.rename(os.path.join(BUFFER_DIR, BUFFER_FILE_NAME), os.path.join(BUFFER_DIR, 'buffer_original'))
 # Open new socket for Alice and perform DHKE
-socket_alice, aes = setup('bob', BUFFER_DIR, BUFFER_FILE_NAME)
+socket_alice, aes_alice = setup('bob', BUFFER_DIR, BUFFER_FILE_NAME)
 dialog.think('Eve thinks: "Hehe, it\'s working!"')
 
 if flag == "--relay":
     # No MitM attack
-    from_Alice = receive_and_decrypt(aes, socket_alice)
+    from_Alice = receive_and_decrypt(aes_alice, socket_alice)
     dialog.chat('Alice slurred: "{}"'.format(from_Alice))
-    encrypt_and_send(from_Alice, aes, socket_bob)
+    encrypt_and_send(from_Alice, aes_bob, socket_bob)
 
-    from_Bob = receive_and_decrypt(aes, socket_bob)
+    from_Bob = receive_and_decrypt(aes_bob, socket_bob)
     dialog.chat('Bob slurred: "{}"'.format(from_Bob))
-    encrypt_and_send(from_Bob, aes, socket_alice)
+    encrypt_and_send(from_Bob, aes_alice, socket_alice)
 elif flag == "--break-heart":
     # Change messages
-    from_Alice = receive_and_decrypt(aes, socket_alice)
+    from_Alice = receive_and_decrypt(aes_alice, socket_alice)
     dialog.chat('Alice slurred: "{}"'.format(from_Alice))
     to_Bob = 'I hate you!'   
-    encrypt_and_send(to_Bob, aes, socket_bob)
+    encrypt_and_send(to_Bob, aes_bob, socket_bob)
 
-    from_Bob = receive_and_decrypt(aes, socket_bob)
+    from_Bob = receive_and_decrypt(aes_bob, socket_bob)
     dialog.chat('Bob slurred: "{}"'.format(from_Bob))
     to_Alice = 'You broke my heart...'
-    encrypt_and_send(to_Alice, aes, socket_alice)
+    encrypt_and_send(to_Alice, aes_alice, socket_alice)
 elif flag == "--custom":
     # User input a message to terminal
-    from_Alice = receive_and_decrypt(aes, socket_alice)
+    from_Alice = receive_and_decrypt(aes_alice, socket_alice)
     dialog.chat('Alice slurred: "{}"'.format(from_Alice))
     dialog.think('Input what you would like Alice to say to Bob')
     to_Bob = input()
-    encrypt_and_send(to_Bob, aes, socket_bob)
+    encrypt_and_send(to_Bob, aes_bob, socket_bob)
 
-    from_Bob = receive_and_decrypt(aes, socket_bob)
+    from_Bob = receive_and_decrypt(aes_bob, socket_bob)
     dialog.chat('Bob slurred: "{}"'.format(from_Bob))
     dialog.think('Input what you would like Bob to say to Alice')
     to_Bob = input()
-    encrypt_and_send(from_Bob, aes, socket_alice)
+    encrypt_and_send(from_Bob, aes_alice, socket_alice)
 # else:
 #     raise
 
