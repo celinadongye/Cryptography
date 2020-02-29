1b8b41e871f1954f3f932fa6a6ec7e5f025745cc
celinadong <celinady25@gmail.com>
2020-02-29 16:04:52 +0000

    Finished exercise, but submission says incorrect

--- mitm/eve.py
+++ mitm/eve.py
@@ -7,55 +7,56 @@ from const import *
 dialog = Dialog('print')
 player = sys.argv[0].split('.', 1)[0]
 flag = sys.argv[1]
-# whenever Bob sends a message, read it in 'buffer_original'
+
 # Connect with Bob and perform DHKE
 socket_bob, aes_bob = setup('alice', BUFFER_DIR, BUFFER_FILE_NAME)
-
 dialog.think('Eve thinks: "Nice, getting there..."')
+
+# Rename buffer file to avoid race conditions
 os.rename(os.path.join(BUFFER_DIR, BUFFER_FILE_NAME), os.path.join(BUFFER_DIR, 'buffer_original'))
+
 # Open new socket for Alice and perform DHKE
 socket_alice, aes_alice = setup('bob', BUFFER_DIR, BUFFER_FILE_NAME)
 dialog.think('Eve thinks: "Hehe, it\'s working!"')
 
+# No MitM attack
 if flag == "--relay":
-    # No MitM attack
+    # Decrypt message from Alice, encrypt and send it to Bob
     from_Alice = receive_and_decrypt(aes_alice, socket_alice)
     dialog.chat('Alice slurred: "{}"'.format(from_Alice))
     encrypt_and_send(from_Alice, aes_bob, socket_bob)
-
+    # Decrypt response from Bob, encrypt and send it to Alice
     from_Bob = receive_and_decrypt(aes_bob, socket_bob)
     dialog.chat('Bob slurred: "{}"'.format(from_Bob))
     encrypt_and_send(from_Bob, aes_alice, socket_alice)
+
+# Change messages
 elif flag == "--break-heart":
-    # Change messages
+    # Decrypt message from Alice, encrypt hate message and send it to Bob
     from_Alice = receive_and_decrypt(aes_alice, socket_alice)
     dialog.chat('Alice slurred: "{}"'.format(from_Alice))
     to_Bob = 'I hate you!'   
     encrypt_and_send(to_Bob, aes_bob, socket_bob)
-
+    # Decrypt response from Bob, encrypt and send it to Alice
     from_Bob = receive_and_decrypt(aes_bob, socket_bob)
     dialog.chat('Bob slurred: "{}"'.format(from_Bob))
-    to_Alice = 'You broke my heart...'
-    encrypt_and_send(to_Alice, aes_alice, socket_alice)
+    encrypt_and_send(from_Bob, aes_alice, socket_alice)
+
+# Prompt user to input message to terminal
 elif flag == "--custom":
-    # User input a message to terminal
+    # Decrypt message from Alice, encrypt user inputted message and send it to Bob
     from_Alice = receive_and_decrypt(aes_alice, socket_alice)
     dialog.chat('Alice slurred: "{}"'.format(from_Alice))
     dialog.think('Input what you would like Alice to say to Bob')
     to_Bob = input()
     encrypt_and_send(to_Bob, aes_bob, socket_bob)
-
+    # Decrypt response from Alice, encrypt user inputted message and send it to Alice
     from_Bob = receive_and_decrypt(aes_bob, socket_bob)
     dialog.chat('Bob slurred: "{}"'.format(from_Bob))
     dialog.think('Input what you would like Bob to say to Alice')
     to_Bob = input()
     encrypt_and_send(from_Bob, aes_alice, socket_alice)
-# else:
-#     raise
 
-# Close the two sockets
+# Close the two sockets (with Alice and Bob)
 tear_down(socket_bob, BUFFER_DIR, 'buffer_original')
 tear_down(socket_alice, BUFFER_DIR, BUFFER_FILE_NAME)
-
-
-## info: yellow, chat: green, 
